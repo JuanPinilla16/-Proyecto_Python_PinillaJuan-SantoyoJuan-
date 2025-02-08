66e1ba97b02ffc6341c6e6cd313d94a720ebf5d1
JuanSantoyoJ <juanda.c0512@gmail.com>
2025-02-08 16:00:52 -0500

    Diccionarios

--- proyecto.py
+++ proyecto.py
@@ -2,35 +2,72 @@ import json
 
 def abrirJSON():
     dicFinal={}
-    with open("./Estudiantes.json",'r') as openFile:
+    with open("./campers.json",'r') as openFile:
         dicFinal=json.load(openFile)
     return dicFinal
 
 def guardarJSON(dic):
-    with open("./Estudiantes.json",'w') as outFile:
+    with open("./campers.json",'w') as outFile:
         json.dump(dic,outFile)
-########################################################################################################################################
-print("Bienvenido a campuslands, ¿Que eres? (1. Camper, 2. Trainer, 3. Coordinador): ")
-usuario=input("Ingresa: ")
-print("\n")
-
+############################################################################################################################################################
+print("\nBienvenido a campuslands, ¿Que eres?")
+print("1. Camper")
+print("2. Trainer")
+print("3. Coordinador")
+usuario=int(input("Ingrese el número de la opción: "))
 inicio=True
-while inicio == True:
-    print("Bienvenido a campuslands")
-    print("1. Estudiantes")
-    opc=int(input("\nIngresa lo que quieres hacer: "))
-    if opc == 3:
-        print("\n")
-        Estudiantes={}
-        Estudiantes=abrirJSON()
-        for i in range(len(Estudiantes["estudiantes"])):
-            print("\n estudiante #: ", i+1)
-            print("Numero de ID: ", Estudiantes["estudiantes"][i]["# de identificacion"])
-            print("Nombres: ", Estudiantes["estudiantes"][i]["Nombres"])
-            print("Apellidos: ", Estudiantes["estudiantes"][i]["Apellidos"])
-            print("Direccion: ", Estudiantes["estudiantes"][i]["Direccion"])
-            print("Acudiente: ", Estudiantes["estudiantes"][i]["Acudiente"])
-            print("Telefono de contacto (celular y fijo): ", Estudiantes["estudiantes"][i]["Telefono de contacto (celular y fijo)"])
-    elif opc==5:
+while inicio:
+    if usuario==1:
+        print("\nBienvenido Camper")
+        print("¿Qué desea hacer?")
+        print("1. Ver notas")
+        print("2. Ver estado")
+        print("3. Ver salon")
+        opcion=int(input("Ingrese el número de la opción: "))
+        if opcion==1:
+            print("cursos")
+
+    elif usuario==2:
+        print("\nBienvenido Trainer")
+        print("¿Qué desea hacer?")
+        print("1. Ver campers")
+        opcion=int(input("Ingrese el número de la opción: "))
+        if opcion==1:
+            Estudiantes={}
+            Estudiantes=abrirJSON()
+            for i in range(len(Estudiantes["estudiantes"])):
+                print("\nEstudiantes:",i+1)
+                print("ID:",Estudiantes["estudiantes"][i]["ID"])
+                print("Nombre:",Estudiantes["estudiantes"][i]["Nombres"])
+                print("Apellido:",Estudiantes["estudiantes"][i]["Apellidos"])
+                print("Direccion:",Estudiantes["estudiantes"][i]["Direccion"])
+                print("Acudiente:",Estudiantes["estudiantes"][i]["Acudiente"])
+                print("Telefono:",Estudiantes["estudiantes"][i]["Telefono"])
+                print("Estado:",Estudiantes["estudiantes"][i]["Estado"])
+                print("Ruta:",Estudiantes["estudiantes"][i]["Ruta"])
+                print("Salon:",Estudiantes["estudiantes"][i]["Salon"])
+
+    elif usuario==3:
+        print("Bienvenido Coordinador")
+        print("1. Ver campers")
+        print("2. Ver trainers")
+        print
+        opcion=int(input("¿Qué desea hacer?"))
+        if opcion==1:
+            Estudiantes={}
+            Estudiantes=abrirJSON()
+            for i in range(len(Estudiantes["estudiantes"])):
+                print("\nEstudiantes:",i+1)
+                print("ID:",Estudiantes["estudiantes"][i]["ID"])
+                print("Nombre:",Estudiantes["estudiantes"][i]["Nombres"])
+                print("Apellido:",Estudiantes["estudiantes"][i]["Apellidos"])
+                print("Direccion:",Estudiantes["estudiantes"][i]["Direccion"])
+                print("Acudiente:",Estudiantes["estudiantes"][i]["Acudiente"])
+                print("Telefono:",Estudiantes["estudiantes"][i]["Telefono"])
+                print("Estado:",Estudiantes["estudiantes"][i]["Estado"])
+                print("Ruta:",Estudiantes["estudiantes"][i]["Ruta"])
+                print("Salon:",Estudiantes["estudiantes"][i]["Salon"])
+
+    else:
         inicio=False
-        print("Error")
+        print("Opción inválida")
